9301179f6ddeb22afeae9ce57f28695ba665e905
Elson <elsouzalopes@gmail.com>
2024-10-15 19:18:58 -0300

    Iniciando

---
+++ faculdade/ws-imposto/imposto/src/imposto/Person.java
@@ -0,0 +1,20 @@
+package imposto;
+
+public class Person {
+
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	int yearsOld;
+	public int getYearsOld() {
+		return yearsOld;
+	}
+	public void setYearsOld(int yearsOld) {
+		this.yearsOld = yearsOld;
+	}
+}
---
+++ faculdade/ws-imposto/imposto/src/imposto/Program.java
@@ -0,0 +1,10 @@
+package imposto;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
